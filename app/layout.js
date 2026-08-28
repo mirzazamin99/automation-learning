ca10d2593e313e2b5b7628249547c8035b5928c8
mirzazamin99 <mirzazamin@gmail.com>
2026-08-28 17:37:40 +0000

    Add viewport meta tag - fixes mobile font scaling

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,11 @@ export const metadata = {
   title: "automation-learning",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
